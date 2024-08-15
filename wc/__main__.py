6206395210c795f3f151c5631ae5708f0f2a755e
abysspacer <artoriastheprogrammer@gmail.com>
2024-08-15 07:52:41 -0400

    add default behavior and optimize

--- wc/__main__.py
+++ wc/__main__.py
@@ -1,5 +1,5 @@
 import argparse
-from file_details import get_byte_count, get_line_count, get_word_count, get_char_count
+from file_details import get_byte_count, get_line_count, get_word_count, get_char_count, get_file_wc_details
 from report import print_details
 
 # create argument parser
@@ -15,8 +15,17 @@ parser.add_argument("file_path", type=str, help="path to target file")
 # parse the arguments
 args = parser.parse_args()
 
+# default behavior
+if not (args.lines or args.words or args.chars or args.bytes):
+    args.lines = args.words = args.bytes = True
+
 # enlist and sanitize details
-details = [args.lines and get_line_count(args.file_path), args.words and get_word_count(args.file_path), args.chars and get_char_count(args.file_path), args.bytes and get_byte_count(args.file_path), args.file_path]
+details = []
+if (args.lines and args.words) or (args.lines and args.chars) or (args.words and args.chars):
+    line_c, word_c, char_c, byte_c = get_file_wc_details(args.file_path)
+    details = [args.lines and line_c, args.words and word_c, args.chars and char_c, args.bytes and byte_c, args.file_path]
+else:
+    details = [args.lines and get_line_count(args.file_path), args.words and get_word_count(args.file_path), args.chars and get_char_count(args.file_path), args.bytes and get_byte_count(args.file_path), args.file_path]
 details = [x for x in details if x]
 
 # report to user
--- wc/file_details.py
+++ wc/file_details.py
@@ -16,3 +16,19 @@ def get_word_count(file_path):
 def get_char_count(file_path):
     with open(file_path, "r") as fh:
         return len(fh.read()) + get_line_count(file_path)
+
+def get_file_wc_details(file_path):
+    """
+    combines get_byte_count, get_line_count, get_word_count, get_char_count for performance purposes
+    """
+    with open(file_path, "r") as fh:
+        byte_count = get_byte_count(file_path)
+        line_count = 0
+        word_count = 0
+        char_count = 0
+        for line in fh:
+            line_count += 1
+            word_count += len(line.strip().split())
+            char_count += len(line)
+        char_count += line_count
+        return (line_count, word_count, char_count, byte_count)
