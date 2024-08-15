bf420ea13c8968d100da59706335ba09119f9b54
abysspacer <artoriastheprogrammer@gmail.com>
2024-08-15 04:44:48 -0400

    add lines flag

--- wc/__main__.py
+++ wc/__main__.py
@@ -1,5 +1,5 @@
 import argparse
-from file_details import get_byte_count
+from file_details import get_byte_count, get_line_count
 from report import print_details
 
 # create argument parser
@@ -7,11 +7,15 @@ parser = argparse.ArgumentParser(description="Unix wc Recreation by abysspacer")
 
 # add flags
 parser.add_argument("-c", "--bytes", action="store_true", help="count the bytes of the input")
+parser.add_argument("-l", "--lines", action="store_true", help="count the lines of the input")
 parser.add_argument("file_path", type=str, help="path to target file")
 
 # parse the arguments
 args = parser.parse_args()
 
+# enlist and sanitize details
+details = [args.lines and get_line_count(args.file_path), args.bytes and get_byte_count(args.file_path), args.file_path]
+details = [x for x in details if x]
+
 # report to user
-if args.bytes:
-    print_details([get_byte_count(args.file_path), args.file_path])
+print_details(details)
--- wc/file_details.py
+++ wc/file_details.py
@@ -1,3 +1,10 @@
 def get_byte_count(file_path):
     from os import path
     return path.getsize(file_path)
+
+def get_line_count(file_path):
+    count = 0
+    with open(file_path, "r") as fh:
+        for line in fh:
+            count += 1
+    return count
